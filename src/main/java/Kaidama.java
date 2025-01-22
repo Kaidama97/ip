3be80ec902e6f233a347a988fd6ede81d9670020
Kaidama97 <taykaijun97@gmail.com>
2025-01-23 01:45:03 +0800

    Implement indentations and fix bugs

--- src/main/java/Kaidama.java
+++ src/main/java/Kaidama.java
@@ -3,20 +3,21 @@ import java.util.Scanner;
 public class Kaidama {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Kaidama.");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Kaidama.");
 
         while(true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________");
+                sc.close();
                 break;
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + input);
+            System.out.println("    ____________________________________________________________");
         }
 
     }
