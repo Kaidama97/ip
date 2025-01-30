d59c489b1f0dc68db69475b0b5c9d384f5131566
Kaidama97 <taykaijun97@gmail.com>
2025-01-31 02:28:34 +0800

    Implement date parser for deadline input
    - Added `isDateFormat` method to validate `yyyy-MM-dd` format.
    - Added `parseDate` method to parse valid date strings into `LocalDate`.
    - Integrated date parsing logic into `setDeadLine` method.
    - Handled `DateTimeParseException` for invalid date formats.
    - Added error messages for invalid or missing date inputs.
    
    This allows the chatbot to accept and parse dates in the `yyyy-MM-dd` format (e.g., "2019-02-01") for deadline tasks.

--- src/main/java/handler/Parser.java
+++ src/main/java/handler/Parser.java
@@ -5,6 +5,11 @@ import task.Events;
 import task.Task;
 import task.ToDos;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Parser {
 
     public static Task inputToTask(String input) {
@@ -18,10 +23,24 @@ public class Parser {
         if (input.startsWith("T")) {
             return new ToDos(isDone, tokens[2].trim());
         } else if (input.startsWith("D")) {
-            return new Deadlines(isDone, tokens[2].trim(), tokens[3].trim());
+            return new Deadlines(isDone, tokens[2].trim(), parseDate(tokens[3].trim()));
         } else {
             String[] time = tokens[3].split("-");
-            return new Events(isDone, tokens[2].trim(), time[0].trim(), time[1].trim());
+            return new Events(isDone, tokens[2].trim(), Parser.parseDate(time[0].trim()), Parser.parseDate(time[1].trim()));
+        }
+    }
+
+    public static LocalDateTime parseDate(String date) {
+        return LocalDateTime.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+    }
+
+    public static boolean isDateFormat(String dateString) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            LocalDate.parse(dateString, formatter);
+            return true;
+        } catch (DateTimeParseException e) {
+            return false;
         }
     }
 }
--- src/main/java/task/Events.java
+++ src/main/java/task/Events.java
@@ -1,16 +1,19 @@
 package task;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Events extends Task {
 
-    private String from;
-    private String to;
-    public Events(String description, String from, String to) {
+    private LocalDateTime from;
+    private LocalDateTime to;
+    public Events(String description, LocalDateTime from, LocalDateTime to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
-    public Events(boolean isDone, String description, String from, String to) {
+    public Events(boolean isDone, String description, LocalDateTime from, LocalDateTime to) {
         super(isDone, description);
         this.from = from;
         this.to = to;
